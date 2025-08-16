29eed218dd8748fcb0e5ecea47b0ade6249b397f
Yashodhan <80139151+boeing23@users.noreply.github.com>
2025-08-16 12:57:05 -0400

    Time: 1 ms (80.10%) | Memory: 15 MB (20.33%) - LeetSync

--- 1-two-sum/two-sum.cpp
+++ 1-two-sum/two-sum.cpp
@@ -4,22 +4,19 @@ public:
 
 
     vector<int> twoSum(vector<int>& nums, int target) {
-        
-vector<int>ans;
+
+        unordered_map<int,int>seen;
         for(int i=0;i<nums.size();i++)
         {
-            for(int j=i+1;j<nums.size();j++)
-            {
-                if(nums[i]+nums[j]==target)
-                {
-                    ans.push_back(i);
-                    ans.push_back(j);
-                    
-                }
-                
-            }
+            int comp=target-nums[i];
+            if(seen.count(comp)){
+                return {seen[comp],i};
 
+            }
+            seen[nums[i]]=i;
         }
-        return ans;
+        return {};
+        
+
     }
 };
