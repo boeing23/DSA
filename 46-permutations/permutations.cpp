8bec1c0d53da4cf9957e2c4c41a1979b1482dd09
Yashodhan <80139151+boeing23@users.noreply.github.com>
2025-09-14 10:58:35 -0400

    Time: 1 ms (42.63%) | Memory: 10.7 MB (46.89%) - LeetSync

--- 46-permutations/permutations.cpp
+++ 46-permutations/permutations.cpp
@@ -1,34 +1,29 @@
 class Solution {
+    vector<vector<int>>res;
 public:
-void solve(vector<vector<int>>&ans, vector<int>&ds, int frq[],vector<int>&nums)
-{
-    if(ds.size()==nums.size())
-    {
-        ans.push_back(ds);
-    }
-
-    for(int i=0;i<nums.size();i++)
-    {
-        if(!frq[i])
-        {
-            frq[i]=1;
-            ds.push_back(nums[i]);
-            solve(ans,ds,frq,nums);
-            frq[i]=0;
-            ds.pop_back();
-        }
-    }
-}
     vector<vector<int>> permute(vector<int>& nums) {
-        vector<int>ds;
-    vector<vector<int>>ans;
-    int n=nums.size();
-    int frq[n];
-
-    for(int i=0;i<nums.size();i++) frq[i]=0;
-
-    solve(ans,ds,frq,nums);
-    return ans;
+        perm(nums,0);
+        return res;
         
+        
+    }
+
+    void perm(vector<int>& nums, int ind)
+    {
+        if(ind==nums.size()) 
+        {
+            res.push_back(nums);
+            return;
+        }
+
+        for(int i=ind;i<nums.size();i++)
+        {
+            swap(nums[ind],nums[i]);
+            perm(nums,ind+1);
+            swap(nums[ind],nums[i]);
+        }
+
+
+
     }
 };
