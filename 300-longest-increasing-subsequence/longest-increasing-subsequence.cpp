920dc192787938c947351c04f926d38431e3a8c2
Yashodhan <80139151+boeing23@users.noreply.github.com>
2025-09-23 17:48:29 -0400

    Time: 691 ms (5.07%) | Memory: 296.4 MB (30.41%) - LeetSync

--- 300-longest-increasing-subsequence/longest-increasing-subsequence.cpp
+++ 300-longest-increasing-subsequence/longest-increasing-subsequence.cpp
@@ -1,27 +1,34 @@
 class Solution {
-    vector<vector<int>> dp; // dp[i][prev+1]
+    vector<vector<int>>dp;
 public:
     int lengthOfLIS(vector<int>& nums) {
-        int n = (int)nums.size();
-        dp.assign(n, vector<int>(n + 1, -1)); // prev in [-1..n-1] -> [0..n]
-        return dfs(nums, 0, -1);
+
+        dp.assign(nums.size(),vector<int>(nums.size()+1,-1));
+        
+
+        return findSubsequence(nums,0,-1);
+
+        
     }
 
-private:
-    int dfs(const vector<int>& nums, int i, int prev) {
-        if (i == (int)nums.size()) return 0;
+    int findSubsequence(vector<int>& nums,int i, int prev)
+    {
+        if(i>=nums.size()) return 0;
 
-        int &memo = dp[i][prev + 1];
-        if (memo != -1) return memo;
+        if(dp[i][prev+1]!=-1)  return dp[i][prev+1];//dp condition yet to be decided;
+        //what if the small number arrives that condition
+         int take=0;
+        if(prev == -1 ||nums[i]>nums[prev])
+        {
+            take=1+findSubsequence(nums,i+1,i);
 
-        // Skip current
-        int best = dfs(nums, i + 1, prev);
-
-        // Take current if it increases
-        if (prev == -1 || nums[i] > nums[prev]) {
-            best = max(best, 1 + dfs(nums, i + 1, i));
         }
+        
+        int nottake=findSubsequence(nums,i+1,prev);
 
-        return memo = best;
+        return dp[i][prev+1]=max(take,nottake);
     }
+
+
+
 };
