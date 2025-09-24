a71bd3e56e0862df28067dccf64292e7587b8c86
Yashodhan <80139151+boeing23@users.noreply.github.com>
2025-09-24 09:15:25 -0400

    Time: 5 ms (50.67%) | Memory: 11.1 MB (18.62%) - LeetSync

--- 424-longest-repeating-character-replacement/longest-repeating-character-replacement.cpp
+++ 424-longest-repeating-character-replacement/longest-repeating-character-replacement.cpp
@@ -1,40 +1,30 @@
 class Solution {
 public:
     int characterReplacement(string s, int k) {
-
         int res=0;
+
         int l=0;
-        int r=0;
+        unordered_map<char,int>count;
         int maxf=0;
-
-        vector<int>frq(26,0);
-
         for(int r=0;r<s.size();r++)
         {
-            int idx=s[r]-'A';
-            maxf=max(maxf,++frq[idx]);
+            count[s[r]]++;
 
-            while((r-l)+1-maxf>k){
-                --frq[s[l]-'A'];
+             maxf=max(maxf,count[s[r]]);
+
+             while((r-l+1)-maxf>k)
+             {
+                count[s[l]]--;
                 l++;
-                
-                
-            }
-            res=max((r-l)+1,res);
-           
-
-        
-           
-           
-
-
-
-            
-
+             }
+             res=max(r-l+1, res);
 
 
         }
+
         return res;
+
+
         
     }
 };
