66e650000e7cd59f4ed7829f56b5206335770387
Yashodhan <80139151+boeing23@users.noreply.github.com>
2025-09-29 18:23:27 -0400

    Time: 79 ms (6.46%) | Memory: 37.6 MB (5.48%) - LeetSync

--- 49-group-anagrams/group-anagrams.cpp
+++ 49-group-anagrams/group-anagrams.cpp
@@ -1,29 +1,38 @@
 class Solution {
 public:
     vector<vector<string>> groupAnagrams(vector<string>& strs) {
-        unordered_map<string, vector<string>>res;
-        for(string s: strs)
-        {
-            vector<int>count(26,0);
-            for(char c:s)
-            {
-                count[c-'a']++;
 
-            }
-            string key=to_string(count[0]);
-            for(int i=1;i<26;i++)
-            {
-                key+=','+to_string(count[i]);
-            }
-            res[key].push_back(s);
+        unordered_map<string, vector<string>>map;
+        vector<vector<string>>ans;
+        
+
+        for(string s:strs)
+        {
+            vector<int>frqmap(26,0);
+            for(char c:s)
+        {
+            frqmap[c-'a']++;
+
+            
+
         }
 
-    vector<vector<string>>result;
-    for(const auto i:res)
-    {
-        result.push_back(i.second);
-    }
+        string a=to_string(frqmap[0]);
+        for(int i=1;i<26;i++)
+        {
+            a+=','+to_string(frqmap[i]);
+        }
 
-    return result;
+        map[a].push_back(s);
+
+
+
+        }
+for(const auto i:map)
+    {
+        ans.push_back(i.second);
+    }
+        return ans;
+        
     }
 };
