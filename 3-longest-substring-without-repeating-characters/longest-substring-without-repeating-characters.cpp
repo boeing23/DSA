a409d02e07f27f269be2fadb2030d809c6a87349
Yashodhan <80139151+boeing23@users.noreply.github.com>
2025-12-01 13:08:04 -0500

    Time: 19 ms (29.29%) | Memory: 14.4 MB (21.46%) - LeetSync

--- 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.cpp
+++ 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.cpp
@@ -1,23 +1,30 @@
 class Solution {
 public:
     int lengthOfLongestSubstring(string s) {
-        int len=0;
-        unordered_set<char>ch;
-        
-        int left=0;
-        for(int r=0;r<s.size();r++)
-        {
-            while(ch.count(s[r]))
-            {
-                ch.erase(s[left]);
-                left++;
-                
-            }
-            ch.insert(s[r]);
-            len=max(r-left+1,len);
 
+        //so basically it needs to be solved using sliding window
+
+        //so here the template of sliding window is
+
+        int l=0;
+        int res=0;
+        unordered_set<char>seen;
+
+    for(int r=0;r<s.size();r++)
+    {
+        char c=s[r];
+        while(seen.count(c))
+        {
+            seen.erase(s[l]);
+            l++;
         }
 
-        return len;
+        seen.insert(c);
+        res=max(res,r-l+1);
+
+    }
+    return res;
+
+        
     }
 };
