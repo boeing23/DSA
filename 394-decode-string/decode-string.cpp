e5d4918be9d21079e33f5e54ad75dc69dfd59bca
Yashodhan <80139151+boeing23@users.noreply.github.com>
2025-12-02 10:56:50 -0500

    Time: 0 ms (100.00%) | Memory: 9.5 MB (47.40%) - LeetSync

--- 394-decode-string/decode-string.cpp
+++ 394-decode-string/decode-string.cpp
@@ -1,63 +1,58 @@
 class Solution {
 public:
     string decodeString(string s) {
+vector<string>stack;
 
-        vector<string>stack;
 
-        for(char c:s)
+        for(int i=0;i<s.size();i++)
         {
+            char c=s[i];
             if(c!=']')
             {
-                stack.push_back(string(1,c));  //this converts single char c in string
+                stack.push_back(string(1,c));
             }
             else
             {
+                //for example [bc
+
                 string substr="";
                 while(stack.back()!="[")
                 {
                     substr=stack.back()+substr;
                     stack.pop_back();
-
                 }
-                stack.pop_back();    //pop '[';
 
-                //now after opening there can be a digit so
-                string k=""; // this stores the digit
+                stack.pop_back(); //removes '[' 
 
-                while(!stack.empty() && isdigit(stack.back()[0])) //isdigit(arg) take
-                //char as an argument thus the stack.back()[0]) '[0]' is responsible 
-                //for converting it to zero
+                string k="";
+                while(!stack.empty() && isdigit(stack.back()[0]))
                 {
                     k=stack.back()+k;
                     stack.pop_back();
-                    
+
                 }
 
-                int repeatedcount=stoi(k);
+
+                int repeated= stoi(k);
                 string r="";
-
-
-                for(int i=0;i<repeatedcount;i++)
+                for(int i=0; i<repeated; i++)
                 {
                     r+=substr;
 
                 }
+
                 stack.push_back(r);
 
             }
-
-            
-
-
+        }
+string res="";
+        for(const auto&part: stack)
+        {
+            res+=part;
 
         }
-        string res="";
-            for(const string&part:stack)
-            {
-                res+=part;
-            }
 
-            return res;
+        return res;
         
     }
 };
