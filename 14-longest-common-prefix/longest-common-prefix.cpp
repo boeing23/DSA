a5d7cc214c7e95049d95ac5428887bbee30ccc31
Yashodhan <80139151+boeing23@users.noreply.github.com>
2025-12-13 17:40:41 -0500

    Time: 0 ms (100.00%) | Memory: 12.1 MB (31.90%) - LeetSync

--- 14-longest-common-prefix/longest-common-prefix.cpp
+++ 14-longest-common-prefix/longest-common-prefix.cpp
@@ -3,24 +3,24 @@ public:
     string longestCommonPrefix(vector<string>& strs) {
 
         string ans=strs[0];
+        
+
+    for(int i=0;i<strs.size();i++)
+    {
+        int j=0;
+        
+
+        string curr=strs[i];
 
 
-
-        for(int i=1;i<strs.size();i++)
+        while(j<min(ans.length(),strs[i].size()))
         {
-            int j=0;
-
-            while(j<min(ans.length(),strs[i].length()))
-            {
-                if(ans[j]!=strs[i][j]) break;
-
-                j++;
-
-            }
-            ans=ans.substr(0,j);
+            if(ans[j]!=curr[j]) break;
+            j++;
         }
 
+        ans=ans.substr(0,j);
+    }
         return ans;
-        
     }
 };
