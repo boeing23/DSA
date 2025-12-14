b1d6841d9a17dfe96874d553334fd9a22029823b
Yashodhan <80139151+boeing23@users.noreply.github.com>
2025-12-14 08:50:59 -0500

    Time: 4 ms (45.45%) | Memory: 22.8 MB (50.80%) - LeetSync

--- 498-diagonal-traverse/diagonal-traverse.cpp
+++ 498-diagonal-traverse/diagonal-traverse.cpp
@@ -1,47 +1,42 @@
 class Solution {
 public:
     vector<int> findDiagonalOrder(vector<vector<int>>& mat) {
+        
+        // you opnly change direction when you hit the boundary or else you keep going
 
         int direction=1;
 
         int row=mat.size();
-
         int col=mat[0].size();
+
         int r=0;
         int c=0;
-
-
-
-        vector<int>res;
+        vector<int>ans;
 
         while(r<row && c<col)
         {
-            res.push_back(mat[r][c]);
-
-            int nr= r+ (direction == 1 ? -1 : 1);
-            int nc= c+ (direction == 1 ? 1 : -1);
+            ans.push_back(mat[r][c]);
+            int nr= r+(direction==1?-1:1);
+            int nc= c+(direction==1?1:-1);
 
             if(nr<0 || nc<0 || nr>=row || nc>=col)
             {
                 if(direction==1)
                 {
-                    if(c== col-1)
+                    if(c==col-1)
                     {
                         r=r+1;
                     }
                     else
                     {
                         c=c+1;
-
                     }
-
                 }
                 else
                 {
                     if(r==row-1)
                     {
                         c=c+1;
-
                     }
                     else
                     {
@@ -55,26 +50,23 @@ public:
                 }
                 else
                 {
-                    direction =1;
+                    direction=1;
                 }
-                
+
 
             }
-
-            else{
+            else
+            {
                 r=nr;
             c=nc;
 
             }
 
             
+
         }
-
-        return res;
-
-        //if nr<0 then go down, direction =0;
+return ans;
 
 
-        
     }
 };
