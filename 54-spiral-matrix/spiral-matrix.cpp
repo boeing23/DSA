23dbcaec4edecea74f663a2fbfa729eb62e38a28
Yashodhan <80139151+boeing23@users.noreply.github.com>
2025-12-14 12:22:33 -0500

    Time: 0 ms (100.00%) | Memory: 9.3 MB (79.51%) - LeetSync

--- 54-spiral-matrix/spiral-matrix.cpp
+++ 54-spiral-matrix/spiral-matrix.cpp
@@ -3,43 +3,45 @@ public:
     vector<int> spiralOrder(vector<vector<int>>& matrix) {
 
         int left=0;
-        int top=0;
         int right=matrix[0].size();
+        int top=0;
         int bottom=matrix.size();
+        vector<int>ans;
 
-        vector<int>res;
 
-
-        while(top<bottom && right>left)
+        while(top<bottom && left<right)
         {
             for(int i=left;i<right;i++)
             {
-
-                res.push_back(matrix[top][i]);
-
+                ans.push_back(matrix[top][i]);
             }
+
             top++;
 
             for(int i=top;i<bottom;i++)
             {
-                res.push_back(matrix[i][right-1]);
+                ans.push_back(matrix[i][right-1]);
             }
+
             right--;
-            if (!(left < right && top < bottom)) {
-                break;
-            }
+            if (!(top < bottom && left < right)) break;
             for(int i=right-1;i>=left;i--)
             {
-                res.push_back(matrix[bottom-1][i]);
+                ans.push_back(matrix[bottom-1][i]);
             }
             bottom--;
             for(int i=bottom-1;i>=top;i--)
             {
-                res.push_back(matrix[i][left]);
+                ans.push_back(matrix[i][left]);
             }
+
             left++;
+
         }
-return res;
+
+
+        return ans;
+
 
         
     }
