1e2fc9fb12b5942e11de4378ab73bea105c65eac
Yashodhan <80139151+boeing23@users.noreply.github.com>
2025-12-14 10:39:43 -0500

    Time: 3 ms (90.01%) | Memory: 23.9 MB (59.57%) - LeetSync

--- 56-merge-intervals/merge-intervals.cpp
+++ 56-merge-intervals/merge-intervals.cpp
@@ -2,34 +2,27 @@ class Solution {
 public:
     vector<vector<int>> merge(vector<vector<int>>& intervals) {
 
-        // sort them based on to their head so that the closer intervals remain closer.
-
-        // [1,3] [2,6] [8,10] [15,18]
-
-        // okay now
-
-        sort(intervals.begin(),intervals.end());
-
-        //okay now closer elements stay close
-
-        // now traverse 
         vector<vector<int>>ans;
 
-        for(auto interval: intervals)
+        sort(intervals.begin(), intervals.end());
+
+        for(auto& interval: intervals)
         {
-            if(ans.empty() || ans.back()[1]<interval[0])
+            if(ans.empty() || interval[0]>ans.back()[1])
             {
                 ans.push_back(interval);
             }
             else
             {
-                ans.back()[1]=max(ans.back()[1],interval[1]);
+                ans.back()[1] = max(ans.back()[1], interval[1]);
             }
+            
+
+
+            
         }
 
         return ans;
-
-
         
     }
 };
