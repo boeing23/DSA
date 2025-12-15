6f7a511f0219d3867440ebe7b1511a56369218d1
Yashodhan <80139151+boeing23@users.noreply.github.com>
2025-12-15 07:37:28 -0500

    Time: 0 ms (100.00%) | Memory: 15.3 MB (31.75%) - LeetSync

--- 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.cpp
+++ 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.cpp
@@ -1,31 +1,45 @@
 class Solution {
 public:
     int search(vector<int>& nums, int target) {
-        int l = 0, r = (int)nums.size() - 1;
+        // the logic is pretty clear, check which half is sorted and do a bs there
 
-        while (l <= r) {
-            int mid = l + (r - l) / 2;
 
-            if (nums[mid] == target) return mid;
+        int l=0;
+        int r=nums.size()-1;
 
-            // Left half sorted
-            if (nums[l] <= nums[mid]) {
-                if (nums[l] <= target && target < nums[mid]) {
-                    r = mid - 1;          // go left
-                } else {
-                    l = mid + 1;          // go right
+        while(l<=r)
+        {
+            int mid=l+(r-l)/2;
+
+            if(nums[mid]==target) return mid;
+
+            if(nums[mid]>=nums[l])
+            {
+                if(nums[l]<=target && target<nums[mid])
+                {
+                    r=mid-1;
+                }
+                else
+                {
+                    l=mid+1;
                 }
             }
-            // Right half sorted
-            else {
-                if (nums[mid] < target && target <= nums[r]) {
-                    l = mid + 1;          // go right
-                } else {
-                    r = mid - 1;          // go left
+
+            else
+            {
+                if(nums[mid]<target && nums[r]>=target)
+                {
+                    l=mid+1;
+                }
+                else
+                {
+                    r=mid-1;
                 }
             }
         }
 
         return -1;
+
+        
     }
 };
