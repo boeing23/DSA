ec692aa220d6025219fc93b6c1fe271afc41d787
Yashodhan <80139151+boeing23@users.noreply.github.com>
2025-12-16 08:30:18 -0500

    Time: 4 ms (69.74%) | Memory: 43.1 MB (44.77%) - LeetSync

--- 621-task-scheduler/task-scheduler.cpp
+++ 621-task-scheduler/task-scheduler.cpp
@@ -2,52 +2,59 @@ class Solution {
 public:
     int leastInterval(vector<char>& tasks, int n) {
 
+        // first get the count of each task and maxHeap to keeep track of most freq tasks
+
 
         vector<int>count(26,0);
 
-        for(char c: tasks)
+        for(int i=0; i<tasks.size();i++)
         {
-            count[c-'A']++;
+            count[tasks[i]-'A']++;
         }
-
         priority_queue<int>maxHeap;
 
-        queue<pair<int,int>>q;
-        int time=0;
-
-        for(int cnt:count)
+        for(int i=0;i<26;i++)
         {
-            if(cnt>0) maxHeap.push(cnt);
+            if(count[i]>0)
+            {
+                maxHeap.push(count[i]);
+            }
         }
+
+        int time=0;
+        queue<pair<int,int>>q;
 
 
         while(!maxHeap.empty() || !q.empty())
         {
             time++;
 
-            if (maxHeap.empty()) {
-                time = q.front().second;
+            if(maxHeap.empty())
+            {
+                time=q.front().second;
             }
             else
             {
                 int cnt=maxHeap.top()-1;
-
                 maxHeap.pop();
+                if(cnt>0)
+                {
+                    q.push({cnt,time+n});
 
-                if(cnt>0) q.push({cnt,time+n});
+                }
+
+                
+
             }
-
             if(!q.empty() && q.front().second==time)
             {
-                maxHeap.push(q.front().first);
+                int front=q.front().first;
                 q.pop();
+                maxHeap.push(front);
             }
-
-        
-
         }
 
+
         return time;
-        
     }
 };
