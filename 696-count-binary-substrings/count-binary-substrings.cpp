bca197acd8a1f93690f7d9bd1859b17f5f540dc1
Yashodhan <80139151+boeing23@users.noreply.github.com>
2025-12-16 19:07:26 -0500

    Time: 6 ms (36.51%) | Memory: 12.9 MB (97.17%) - LeetSync

--- 696-count-binary-substrings/count-binary-substrings.cpp
+++ 696-count-binary-substrings/count-binary-substrings.cpp
@@ -1,31 +1,30 @@
 class Solution {
 public:
     int countBinarySubstrings(string s) {
-        int prevCon=0;
+
         int currCon=1;
+        int prevCon=0;
         int ans=0;
 
         for(int i=1;i<s.size();i++)
         {
             if(s[i]!=s[i-1])
             {
-                ans+=min(prevCon, currCon);
+                ans+=min(prevCon,currCon);
 
                 prevCon=currCon;
                 currCon=1;
-            }
 
+            }
             else
             {
-                currCon++;
-
+                 currCon++;
             }
-
-            
         }
 
-        ans+=min(prevCon, currCon);
-        return ans;
+        ans+=min(currCon, prevCon);
 
+        return ans;
+        
     }
 };
