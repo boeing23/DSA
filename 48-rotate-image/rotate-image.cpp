bc581df66513dacb4d12c82a7c5580db54e29b5e
Yashodhan <80139151+boeing23@users.noreply.github.com>
2025-12-17 08:27:41 -0500

    Time: 0 ms (100.00%) | Memory: 10.1 MB (68.38%) - LeetSync

--- 48-rotate-image/rotate-image.cpp
+++ 48-rotate-image/rotate-image.cpp
@@ -2,26 +2,18 @@ class Solution {
 public:
     void rotate(vector<vector<int>>& matrix) {
 
-
-        //we are supposed to rotate an image. 
-        //in place rotation which means there should be a point to swap the values;
-        //how to transpose the matrix
-    //so here I had a problem of how to shift digonally
-
-    for(int i=0;i<matrix.size();i++)
-    {
-        for(int j=0;j<i;j++)  //j less than i ensures it do not swap the same element for example consider a scenarion i=j then?
+        for(int i=0;i<matrix.size();i++)
         {
-            swap(matrix[i][j], matrix[j][i]);
+            for(int j=i;j<matrix.size();j++)
+            {
+                swap(matrix[i][j], matrix[j][i]);
+            }
         }
 
-
-    }
-
-    for(int i=0;i<matrix.size();i++)
-    {
-        reverse(matrix[i].begin(),matrix[i].end());
-    }
+        for(int i=0;i<matrix.size();i++)
+        {
+            reverse(matrix[i].begin(), matrix[i].end());
+        }
         
     }
 };
