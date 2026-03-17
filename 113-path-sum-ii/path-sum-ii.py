4811fd838d8d0510806cde3bfaf12f25f958fe38
Yashodhan <80139151+boeing23@users.noreply.github.com>
2026-03-17 01:38:21 -0400

    Time: 3 ms (40.54%) | Memory: 23.7 MB (18.61%) - LeetSync

--- 113-path-sum-ii/path-sum-ii.py
+++ 113-path-sum-ii/path-sum-ii.py
@@ -6,19 +6,25 @@
 #         self.right = right
 class Solution:
     def pathSum(self, root: Optional[TreeNode], targetSum: int) -> List[List[int]]:
+
+        # basically in this you have to take a path and if the pathsum==0 then 
+        # add that path to the result
+        # now here too you go to each node and append the path
+        # so in this question the catch is if you append it early on then you pass the 
+        # mutated value to the recursive call
         path=[]
         res=[]
 
-        def dfs(node,path,remaining):
+        def dfs(node,remaining,path):
             if not node:
-                return
+                return 0
             if not node.left and not node.right and remaining==node.val:
                 res.append(path+[node.val])
-            dfs(node.left, path+[node.val], remaining-node.val)
-            dfs(node.right, path+[node.val], remaining-node.val)
-        dfs(root, path, targetSum)
+                print(path)
+            dfs(node.left,remaining-node.val, path+[node.val])
+
+            
+            dfs(node.right,remaining-node.val, path+[node.val])
+        dfs(root, targetSum, path)
         return res
-
-
-
         
