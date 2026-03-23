fbfc114d11933f334205abdf24b50e402765a2fe
Yashodhan <80139151+boeing23@users.noreply.github.com>
2026-03-22 21:13:27 -0400

    Time: 0 ms (100.00%) | Memory: 19.5 MB (20.77%) - LeetSync

--- 91-decode-ways/decode-ways.py
+++ 91-decode-ways/decode-ways.py
@@ -1,31 +1,43 @@
 class Solution:
     def numDecodings(self, s: str) -> int:
 
-        # so there could be multiple ways to decode a string
-        # in one you can take a single digit or take a doublt digit
-        # and the val of double should not be > 26
 
-        # ways variable
-        # traverse the string
+        # this is basically a typical take not take question for each step i have to store the possible outcome
+
+
+        # base condn=
+
+        # if i>=len(s):
+        #     return 1      # it means if the goal state reached that means you have arrived to the soln
+
+        # if s[i]=='0':
+        #     return 0
+        # take1=dfs(i+1)
+
+        # if s[i]==2 and s[i]<7:
+        #     take+=dfs(i+2)
+        # return 
+
         dp=defaultdict(int)
-        @cache
-        def dfs(index:int):
+
+        def dfs(index):
             if index==len(s):
                 return 1
             if s[index]=='0':
                 return 0
-            # if index in dp:
-            #     return dp[index]
-            
+            if index in dp:
+                return dp[index]
             dp[index]+=dfs(index+1)
 
             if index<len(s)-1:
                 if s[index]=='1' or (s[index]=='2' and s[index+1]<'7'):
                     dp[index]+=dfs(index+2)
             return dp[index]
-            
-
         return dfs(0)
+
+
+
+
         
         
         
