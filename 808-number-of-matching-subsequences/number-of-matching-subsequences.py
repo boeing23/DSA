51bbb620fa6b3d57644e65f9d68d06a5c2afd06c
Yashodhan <80139151+boeing23@users.noreply.github.com>
2026-04-10 13:55:42 -0400

    Time: 1099 ms (15.35%) | Memory: 23.2 MB (15.19%) - LeetSync

--- 808-number-of-matching-subsequences/number-of-matching-subsequences.py
+++ 808-number-of-matching-subsequences/number-of-matching-subsequences.py
@@ -1,28 +1,60 @@
 class Solution:
     def numMatchingSubseq(self, s: str, words: List[str]) -> int:
 
-
-        # so basicaly go to each word and check whether it is in s if it is
-        # then it is the start
-        # and then traverse it checking the index current is bigger than the 
-        # index where you started
+        # not the optimal approach would be finding the pos using the binary search
+        # then checking if the mid is greater than pos if it is then change pos to
+        # the mid index and then set found for that char
 
 
-        # the brute force would be using s.find 
+
+        # but first store all the indices of the char in s
+
+
+        index_map=defaultdict(list)
         count=0
+
+        for i in range(len(s)):
+            index_map[s[i]].append(i)
+
+        # this creates the index map
+
+        # now in that list we have to do the binary search
 
         for word in words:
             pos=-1
             found=True
 
-
             for ch in word:
-                pos=s.find(ch, pos+1)
-
-                if pos==-1:
+                if ch not in index_map:
                     found=False
                     break
+                indexes=index_map[ch]
+
+                lo=0
+                hi=len(indexes)-1
+                res=-1
+
+                while lo<=hi:
+
+                    mid=(lo+hi)//2
+
+                    if indexes[mid] >=pos+1:
+                        res=mid
+                        hi=mid-1
+                    else:
+                        lo=mid+1
+                if res==-1:
+                    found=False
+                    break
+                pos=indexes[res]
+
             if found==True:
                 count+=1
         return count
+        
+
+
+
+
+
         
