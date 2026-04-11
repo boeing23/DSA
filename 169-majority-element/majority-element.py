2961cf70c783724eb573de38cef58922ead43cf7
Yashodhan <80139151+boeing23@users.noreply.github.com>
2026-04-11 10:07:25 -0400

    Time: 3 ms (86.18%) | Memory: 21.1 MB (91.80%) - LeetSync

--- 169-majority-element/majority-element.py
+++ 169-majority-element/majority-element.py
@@ -1,10 +1,31 @@
 class Solution:
     def majorityElement(self, nums: List[int]) -> int:
 
-        frqMap=Counter(nums)
+        # frqMap=Counter(nums)
 
-        for key,val in frqMap.items():
-            if val > len(nums)//2:
-                return key
-        return 0 
+        # for key,val in frqMap.items():
+        #     if val > len(nums)//2:
+        #         return key
+        # return 0 
+
+
+        # Boyer Moore algo,
+        # if you are on current num, take this as a candidate and keep counting the votes for
+        # if you come across any different candi then remove the count if count==0 then change the candiate
+
+
+        candidate=None
+        count=0
+
+        for n in nums:
+            if count==0:
+                candidate=n
+                count+=1
+            else:
+                if n!=candidate:
+                    count-=1
+                else:
+                    count+=1
+
+        return candidate
         
