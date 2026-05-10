c1d68ef9010a76987bb80beb29f444424eb845bd
Yashodhan <80139151+boeing23@users.noreply.github.com>
2026-05-10 16:07:55 -0400

    Time: 0 ms (100.00%) | Memory: 23.1 MB (38.00%) - LeetSync

--- 56-merge-intervals/merge-intervals.py
+++ 56-merge-intervals/merge-intervals.py
@@ -1,20 +1,28 @@
 class Solution:
     def merge(self, intervals: List[List[int]]) -> List[List[int]]:
-        # sort with the first element
-        # then all the starts come together 
-        # 1,3] 2,6] 8,10] 15,18]
-        intervals.sort()
 
-        #now traverse through each interval
+
+        # sort them as per their start
+
+
+        intervals.sort(key=lambda x:x[0])
+
         ans=[]
-        for i in range(len(intervals)):
-            if len(ans)==0 or intervals[i][0]>ans[-1][1]:
-                ans.append(intervals[i])
+
+        for start,end in intervals:
+
+            if ans and ans[-1][1]>= start:
+                ans[-1][1]=max(ans[-1][1], end)
             else:
-                ans[-1][1]=max(intervals[i][1], ans[-1][1])
+
+                ans.append([start,end])
+
         return ans
 
 
-        
+
+
+
+
 
         
