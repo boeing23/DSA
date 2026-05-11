55ee521a90300d0d2101b11c976bc26d4692a6b4
Yashodhan <80139151+boeing23@users.noreply.github.com>
2026-05-11 19:05:34 -0400

    Time: 242 ms (64.20%) | Memory: 21.5 MB (72.23%) - LeetSync

--- 200-number-of-islands/number-of-islands.py
+++ 200-number-of-islands/number-of-islands.py
@@ -2,50 +2,32 @@ class Solution:
     def numIslands(self, grid: List[List[str]]) -> int:
 
 
-        # now in this we are supposed to mark the islands and get total
-        # islands in this
+        q=deque()
+        islands=0
 
-        # one way to do it could be a dfs approach, where we start from an island
-        # which is 1 and then from there we do a dfs and whichever are the connected ones
-        # we mark them '0' so that the we don't recount that piece of land again
-
-
-        # now what would be the tc and sc for this.
-
-        # as we are doing a dfs the tc would be n*n as we are visiting each and every land
-        # then space complexity be constant as we are not using an extra space here
-        # as we just update the matrix in the place
-
-        def depthfirstsearch(r: int,c: int)->None:
-            # base cases will basically keep track of boundary so that we don
-            # t go out of bounds and we also check if the new one which has come it is a land   
-            if 0>r or r>=len(grid) or 0>c or c>=len(grid[0]) or grid[r][c]!='1':
-                return
-            grid[r][c]='0'
-
-            for nr,nc in directions:
-                newR=r+nr
-                newC=c+nc
-
-                if 0<=newR<len(grid) and 0<=newC<len(grid[0]) and grid[newR][newC]=='1':
-                    depthfirstsearch(newR,newC)
-            
-
-
-
-        rows= len(grid)
-        cols=len(grid[0])
-        islands=0 # for counting number of islands
-        directions=[[1,0],[-1,0],[0,1],[0,-1]]
-
-        for r in range(rows):
-            for c in range(cols):
-                # you do a dfs only when you encounter a land
-
+        for r in range(len(grid)):
+            for c in range(len(grid[0])):
                 if grid[r][c]=='1':
-                    depthfirstsearch(r,c)
-                    islands+=1     #whenever we come out of the call we have marked the connected lands and it forms a single island
+                    islands+=1
+                    q.append([r,c])
+                    grid[r][c]='0'
 
+                    while q:
+
+                        row,col=q.popleft()
+
+                        # grid[row][col]='0'
+
+                        for dr,dc in [(1,0),(-1,0),(0,1),(0,-1)]:
+                            nr=row+dr
+                            nc=col+dc
+
+                            if 0<=nr<len(grid) and 0<=nc<len(grid[0]) and grid[nr][nc]=='1':
+                                grid[nr][nc]='0'
+                                q.append([nr,nc])
         return islands
+
+
+
 
         
