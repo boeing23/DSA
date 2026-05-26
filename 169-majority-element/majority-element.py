793970dd23721bdc8a28fb978415db634d3a6903
Yashodhan <80139151+boeing23@users.noreply.github.com>
2026-05-26 12:26:24 -0400

    Time: 7 ms (58.18%) | Memory: 21.1 MB (52.17%) - LeetSync

--- 169-majority-element/majority-element.py
+++ 169-majority-element/majority-element.py
@@ -1,31 +1,20 @@
 class Solution:
     def majorityElement(self, nums: List[int]) -> int:
 
-        # frqMap=Counter(nums)
 
-        # for key,val in frqMap.items():
-        #     if val > len(nums)//2:
-        #         return key
-        # return 0 
+        # it is something like this
+        # basically you keep incrementing if equal
 
-
-        # Boyer Moore algo,
-        # if you are on current num, take this as a candidate and keep counting the votes for
-        # if you come across any different candi then remove the count if count==0 then change the candiate
-
-
+        # 3 but suddenly a new elemt appears you do
+        # -1
+        # if ==0 majority_cou
         candidate=None
         count=0
-
         for n in nums:
             if count==0:
                 candidate=n
-                count+=1
-            else:
-                if n!=candidate:
-                    count-=1
-                else:
-                    count+=1
-
+            count+=1 if n==candidate else -1
         return candidate
+
+            
         
