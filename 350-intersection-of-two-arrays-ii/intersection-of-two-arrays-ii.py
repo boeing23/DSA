dc688dfb404988a00c86f3d2f530f653a8ad2a2c
Yashodhan <80139151+boeing23@users.noreply.github.com>
2026-05-28 20:01:42 -0400

    Time: 0 ms (100.00%) | Memory: 19.4 MB (55.52%) - LeetSync

--- 350-intersection-of-two-arrays-ii/intersection-of-two-arrays-ii.py
+++ 350-intersection-of-two-arrays-ii/intersection-of-two-arrays-ii.py
@@ -1,14 +1,17 @@
 class Solution:
     def intersect(self, nums1: List[int], nums2: List[int]) -> List[int]:
 
-
-        frqMap=Counter(nums1)
+        count1=Counter(nums1)
+        count2=Counter(nums2)
         ans=[]
 
-        for n in nums2:
 
-            if frqMap.get(n,0)>0:
-                ans.append(n)
-                frqMap[n]-=1
+        for n in count1:
+            if n in count1 and n in count2:
+                frq=min(count1[n],count2[n])
+                while frq:
+                    ans.append(n)
+                    frq-=1
         return ans
+
         
