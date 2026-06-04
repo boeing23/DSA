e54b4bdda179924fcea27dbea434eee898489600
Yashodhan <80139151+boeing23@users.noreply.github.com>
2026-06-04 15:09:44 -0400

    Time: 99 ms (85.91%) | Memory: 77 MB (53.87%) - LeetSync

--- 146-lru-cache/lru-cache.py
+++ 146-lru-cache/lru-cache.py
@@ -1,27 +1,59 @@
+class Node:
+    def __init__(self,key, value):
+        self.key=key
+        self.value=value
+        self.next=None
+        self.prev=None
 class LRUCache:
 
     def __init__(self, capacity: int):
-        self.capacity= capacity
-        self.cache=OrderedDict()
+        self.cache={}
+        self.capacity=capacity
+        self.left=Node(0,0)
+        self.right=Node(0,0)
+        self.left.next=self.right
+        self.right.prev=self.left
+    def _insert(self, node):
+        prev=self.right.prev
+        prev.next=node
+        node.next=self.right
+        node.prev=prev
+        self.right.prev=node
+    def _remove(self,node):
+        prev=node.prev
+        next=node.next
+
+        prev.next=next
+        next.prev=prev
+        
 
     def get(self, key: int) -> int:
-
         if key not in self.cache:
             return -1
-        self.cache.move_to_end(key)
-        return self.cache[key]
+        node=self.cache[key]
+
+        self._remove(node)
+        self._insert(node)
+        return node.value
         
 
     def put(self, key: int, value: int) -> None:
         if key in self.cache:
-            self.cache[key]=value
-            self.cache.move_to_end(key)
+            node=self.cache[key]
+            node.value=value
+            self._remove(node)
+            self._insert(node)
+
         else:
-            self.cache[key]=value
-            if len(self.cache)>self.capacity:
-                self.cache.popitem(last=False)
-    
+            self.cache[key]=Node(key,value)
         
+            self._insert(self.cache[key])
+            if len(self.cache)>self.capacity:
+                node=self.left.next
+                self._remove(node)
+                del self.cache[node.key]
+            
+            
         
 
 
